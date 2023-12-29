e345d8d878888c310bb517d989b622c295bf0486
roy-stephanie <korettex@gmail.com>
2023-12-29 10:31:55 +0100

    Add Important Comment

--- src/js/utils/extractArrayToArray/ExtractArrayToArray.js
+++ src/js/utils/extractArrayToArray/ExtractArrayToArray.js
@@ -1,15 +1,25 @@
+// This function takes an array of recipes, a property name to extract data (nameDataExtract),
+// and an optional second property name (secondNameDataExtract) for nested extraction.
 export default function ExtractArrayToArray(recipes, nameDataExtract, secondNameDataExtract = undefined) {
+    // Map through the recipes array to extract the specified property.
     const newArray = recipes.map(recipe => {
+        // Extract the first property based on the provided nameDataExtract.
         let firstProperty = recipe[nameDataExtract];
 
+        // Check if a second property is specified for nested extraction.
         if (secondNameDataExtract) {
+            // If specified, map through the first property to extract the second property.
             return firstProperty.map(ingredient => {
-                return ingredient[secondNameDataExtract]
+                return ingredient[secondNameDataExtract];
             });
         }
+        // If no second property is specified, return the extracted first property directly.
         return firstProperty;
     });
 
+    // Flatten the array of extracted properties.
     const flatNewArray = newArray.flat();
+
+    // Use a set to ensure unique values and return the result.
     return [...new Set(flatNewArray)];
 }
